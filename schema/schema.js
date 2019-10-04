d4ce3d8282d1e62f3a8543cce045b062ab35c60e
Sebuliba-Adrian <adrian.sebuliba1@gmail.com>
2019-10-04 23:33:55 +0300

    Query a book with its associated author

--- schema/schema.js
+++ schema/schema.js
@@ -1,34 +1,72 @@
 const graphql = require("graphql");
-const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLInt
+} = graphql;
 
+var authors = [
+  { name: "Patrick Rothfuss", age: 44, id: "1" },
+  { name: "Brandon Sanderson", age: 42, id: "2" },
+  { name: "Terry Pratchett", age: 66, id: "3" }
+];
 //dummy data
-var books =[
-    {name: 'Name of the Wind', genre: 'Fantasy', id:'1'},
-    { name: 'The Final Empire', genre: 'Fantasy', id: '2'},
-    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' }
-]
-const BookType = new GraphQLObjectType({ //create book time
+var books = [
+  { name: "Name of the Wind", genre: "Fantasy", id: "1", authorId: "1" },
+  { name: "The Final Empire", genre: "Fantasy", id: "2", authorId: "2" },
+  { name: "The Long Earth", genre: "Sci-Fi", id: "3", authorId: "3" }
+];
+
+const AuthorType = new GraphQLObjectType({
+  //create author
+  name: "Author",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt }
+  })
+});
+const BookType = new GraphQLObjectType({
+  //create book time
   name: "Book",
   fields: () => ({
-      id: { type: GraphQLID },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
-    genre: { type: GraphQLString }
+    genre: { type: GraphQLString },
+    author: {
+      type: AuthorType,
+      resolve(parent, args) {
+        console.log(parent);
+        return authors.find(author => author.id == parent.authorId);
+      }
+    }
   })
 });
 
-const RootQuery = new GraphQLObjectType({ // How we jump into graph
-  name: 'RootQueryType',
+const RootQuery = new GraphQLObjectType({
+  // How we jump into graph
+  name: "RootQueryType",
   fields: {
     book: {
       type: BookType,
-          args: { id: { type: GraphQLID } },
-      resolve(parent, args){
-          return books.find(book => book.id == args.id);
-          //code to get data from db / other source
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        //parent is the book in this case
+        return books.find(book => book.id == args.id);
+        //code to get data from db / other source
+      }
+    },
+    author: {
+      type: AuthorType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return authors.find(author => author.id == args.id);
       }
     }
   }
 });
 module.exports = new GraphQLSchema({
-    query: RootQuery
-})
+  query: RootQuery
+});
