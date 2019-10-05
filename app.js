4ffa09ab4189fa5869671f9a43264fcbdef589bf
Sebuliba-Adrian <adrian.sebuliba1@gmail.com>
2019-10-05 11:41:24 +0300

    add cors middleware

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const graphqlHttp = require("express-graphql");
 const schema = require("./schema/schema");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 app = express();
 
+app.use(cors());
 const uri = "mongodb+srv://adrian:Qwerty123!@cluster0-jxq2l.mongodb.net/graphql-db?retryWrites=true&w=majority";
 mongoose.connect(uri, {useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: true});
 mongoose.connection.once('open', ()=>{
