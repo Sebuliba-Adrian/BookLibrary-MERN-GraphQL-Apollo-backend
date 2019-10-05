f0e3344204378005c393c0e0a42fc0505a80d3a9
Sebuliba-Adrian <adrian.sebuliba1@gmail.com>
2019-10-05 05:49:21 +0300

    setup mongoose to communicate with mongodb atlas

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-const express = require('express');
-const graphqlHttp = require('express-graphql');
-const schema = require('./schema/schema');
+const express = require("express");
+const graphqlHttp = require("express-graphql");
+const schema = require("./schema/schema");
+const mongoose = require("mongoose");
 
 app = express();
 
-app.use('/graphql', graphqlHttp({
- schema : schema,
- graphiql: true
-}));
+const uri = "mongodb+srv://adrian:Qwerty123!@cluster0-jxq2l.mongodb.net/admin?retryWrites=true&w=majority";
+mongoose.connect(uri, {useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: true});
+mongoose.connection.once('open', ()=>{
+  console.log("DB Connected!");
+})
+app.use(
+  "/graphql",
+  graphqlHttp({
+    schema: schema,
+    graphiql: true
+  })
+);
 
-app.listen(4000, ()=>{
+app.listen(4000, () => {
   console.log("Server running!");
 });
